49e2cce38c7437a3f6bc99f0a84e5bea358ffd18
albaehaki <zackyalbaehaki@gmail.com>
2024-03-13 23:42:35 +0700

    mengganti nama function

--- src/store/wadotmeStore.ts
+++ src/store/wadotmeStore.ts
@@ -1,17 +1,17 @@
 import { create } from "zustand";
 
-interface SignUp {
+interface WADotMe {
   nomerWA: number;
   pesan: string;
   handleChange: (option: string, data: string) => void;
 }
 
-const useSignUpStore = create<SignUp>()((set) => ({
+const useWADotMeStore = create<WADotMe>()((set) => ({
   nomerWA: 0,
   pesan: "",
   handleChange: (option, data) => {
-    
+
   },
 }));
 
-export default useSignUpStore;
+export default useWADotMeStore;
